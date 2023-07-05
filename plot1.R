bfd35b1c5bfd45da7a3fb0935727bc874cd9261d
snoopyaudrey <121604214+snoopyaudrey@users.noreply.github.com>
2023-07-05 18:33:45 +0200

    plot1.R

--- plot1.R
+++ plot1.R
@@ -5,18 +5,18 @@ setwd("C:/Users/snoop/OneDrive/Documents/CourseraSpecScienceDonnees/4. Explorato
 library("data.table")
 unzip("exdata_data_household_power_consumption.zip")
 
-powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+dta <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
 
-powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+dta[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
-powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+dta[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 
-powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+dta <- dta[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
 png("plot1.png", width=480, height=480)
 
 # Plot 1
-hist(powerDT[, Global_active_power], main="Global Active Power", 
+hist(dta[, Global_active_power], main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Orange")
 
 dev.off()
